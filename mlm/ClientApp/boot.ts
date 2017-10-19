003e796f6f841abde93b6ae032b64ec39c186f4e
Eddy Ekofo <contact@eddyekofo.com>
2017-10-19 22:22:23 +0100

    error: " Failed to mount component: template or render function not defined"

--- mlm/ClientApp/boot.ts
+++ mlm/ClientApp/boot.ts
@@ -7,11 +7,13 @@ Vue.use(VueRouter);
 const routes = [
     { path: '/', component: require('./components/home/home.vue.html') },
     { path: '/counter', component: require('./components/counter/counter.vue.html') },
-    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') }
+    { path: '/fetchdata', component: require('./components/fetchdata/fetchdata.vue.html') },
+    { path: '/messenger', component: require('./components/messenger/messenger.vue.html') }
+
 ];
 
 new Vue({
     el: '#app-root',
     router: new VueRouter({ mode: 'history', routes: routes }),
     render: h => h(require('./components/app/app.vue.html'))
-});
+}).$mount('#app-root')
--- mlm/ClientApp/components/messenger/messenger.ts
+++ mlm/ClientApp/components/messenger/messenger.ts
@@ -1,19 +1,27 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 
-interface Messages{
+interface Messages {
     messageFrom: string;
     messageRecieved: string;
 }
 
 @Component
-export default class FetchMessages extends Vue{
-    messages: Messages[] = [];
-    mounted() {
-        fetch('api/TranslateTest/Messages')
-            .then(response => response.json() as Promise<Messages[]>)
-            .then(data => {
-                this.messages = data;
-            });
+export default class FetchMessages extends Vue {
+    message: string = 'Hello!'
+    // Component methods can be declared as instance methods
+
+    onClick(): void {
+        console.log(this.message)
     }
+
+    // messages: Messages[] = [];
+
+    // mounted() {
+    //     fetch('api/TranslateText/Messages')
+    //         .then(response => response.json() as Promise<Messages[]>)
+    //         .then(data => {
+    //             this.messages = data;
+    //         });
+    // }
 }
