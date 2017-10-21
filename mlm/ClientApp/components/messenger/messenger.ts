36b1cc9c287b04f047cb0831d606b2206ef85cbf
Eddy Ekofo <contact@eddyekofo.com>
2017-10-21 23:21:17 +0100

    Added the place holder with Vuejs

--- mlm/ClientApp/components/messenger/messenger.ts
+++ mlm/ClientApp/components/messenger/messenger.ts
@@ -4,4 +4,8 @@ import { Component } from 'vue-property-decorator';
 @Component
 export default class MessengerComponent extends Vue {
     msg : string = "Hello Vue";
+
+    onClick(): void {
+        window.alert(this.msg)
+    }
 }
